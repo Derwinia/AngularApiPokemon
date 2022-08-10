29cb39b5d9d4ad11a67a735a8d52140e6cd75b95
unknown <steve.lorent@gmail.com>
2022-08-10 12:18:18 +0200

    Menu directives

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -16,9 +16,18 @@ export class NavComponent implements OnInit {
     this.mesLiens = [
       {title : "Home", url : "home"},
       {title : "Demos", children : [
-        {title : "Bindings", url : "demo/demo1"}
+        {title : "Bindings", url : "demo/demo1"},
+        {title : "Pipes", url : "demo/demo2"},
+        {title : "Directives", url : "demo/demo3"}
+      ]},
+      {title : "Exercices", children : [
+        {title : "Chronomètre", url : "exercice/exo1"}
       ]}
     ]
+  }
+
+  toggleChildrenVisible(index : number) {
+    this.mesLiens[index].isChildrenVisible = !this.mesLiens[index].isChildrenVisible
   }
 
 }
